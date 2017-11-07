c60c5eb73216f311b23d7545f89e25ce1780d6cc
Ozgur Zeren <unity100@gmail.com>
2017-11-08 00:26:58 +0100

    Custom Banner meta box and meta saving code removed from metaboxes
    Deprecated function Custom Banners per Post is removed

--- classes/patreon_metabox.php
+++ classes/patreon_metabox.php
@@ -12,8 +12,6 @@ class Patron_Metabox {
 
 		add_action( 'add_meta_boxes', array($this, 'patreon_plugin_meta_boxes') );
 		add_action( 'save_post', array($this, 'patreon_plugin_save_post_class_meta'), 10, 2 );
-		add_action( 'add_meta_boxes', array($this, 'patreon_banner_metabox') );
-		add_action( 'save_post', array($this, 'patreon_post_banner_save') );
 
 	}
 
@@ -88,68 +86,6 @@ class Patron_Metabox {
 		}
 
 		$patreon_level = get_post_meta( $post_id, 'patreon-level', true );
-
-	}
-
-
-	function patreon_banner_metabox($post_type) {
-
-	    $post_types = get_post_types(array('public'=>true),'names');
-
-	    $exclude = array(
-	    	'attachment'
-	    	);
-
-	    if (in_array($post_type,$exclude) == false && in_array($post_type, $post_types)) {
-
-	        add_meta_box(
-	            'patreon-post-banner-meta-box',
-	            'Custom Patreon Banner',
-	            array($this, 'patreon_post_banner_meta_box'),
-	            $post_type,
-	            'normal',
-	            'high'
-	        );
-
-	    }
-
-	}
-
-	function patreon_post_banner_meta_box($post) {
-
-	    $patreon_post_banner = get_post_meta($post->ID, 'patreon_post_banner', true);
-
-	    wp_editor( $patreon_post_banner, 'patreon_post_banner' );
-
-	    wp_nonce_field(
-	        plugin_basename(__FILE__),
-	        'patreon_post_banner_none'
-	    );
-
-	}
-
-	function patreon_post_banner_save($post_id) {
-
-	    if ( ( (isset($_POST['post_type']) && $_POST['post_type'] === 'page') && current_user_can('edit_page', $post_id) ) || current_user_can('edit_post', $post_id) ) {
-
-	        if ((( ! defined('DOING_AUTOSAVE')) || ( ! DOING_AUTOSAVE)) && (( ! defined('DOING_AJAX')) || ( ! DOING_AJAX))) {
-
-	            if (isset($_POST['patreon_post_banner_none']) && wp_verify_nonce($_POST['patreon_post_banner_none'], plugin_basename(__FILE__))) {
-
-	                $patreon_post_banner = wp_kses_post($_POST['patreon_post_banner']);
-
-	                if ($patreon_post_banner !== '') {
-	                    add_post_meta($post_id, 'patreon_post_banner', $patreon_post_banner, true) OR update_post_meta($post_id, 'patreon_post_banner', $patreon_post_banner);
-	                } else {
-	                    delete_post_meta($post_id, 'patreon_post_banner');
-
-	                }
-
-	            }
-
-	        }
-
-	    }
 
 	}
 
