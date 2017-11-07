6e1087eb42a2b58746d23b67aab1f8d3092b79f0
Ozgur Zeren <unity100@gmail.com>
2017-11-07 01:37:31 +0100

    Shortcode and Shortcode function for patreon_content is removed
    Because the post type patreon-content is removed, this shortcode and its function are not used.

--- classes/patreon_shortcodes.php
+++ classes/patreon_shortcodes.php
@@ -10,7 +10,6 @@ class Patreon_Shortcodes {
 
 	function __construct() {
 
-		add_shortcode( 'patreon_content', array($this, 'embedPatreonContent') );
 		add_shortcode( 'patreon_register_button', array($this, 'renderPatreonButton') );
 		add_shortcode( 'patrons_name', array($this, 'renderPatronsName') );
 		add_shortcode( 'personal_message', array($this, 'renderPersonalMessage') );
@@ -112,94 +111,6 @@ class Patreon_Shortcodes {
 
 	}
 
-	public function embedPatreonContent($args) {
-
-		/* check if shortcode has slug parameter */
-		if(isset($args['slug'])) {
-
-			if(isset($args['username'])) {
-
-				$current_user = wp_get_current_user();
-
-				if ( !($current_user instanceof WP_User) ) {
-					// user not logged in, lets pretend the content doesn't exist
-					return 'Patreon content not found.';
-				}
-
-				if($current_user->user_login != $args['username']) {
-					// this content is not for this user
-					return 'This content is exclusive.';
-				}
-
-			}
-
-			/* get patreon-content post with matching url slug */
-			$patreon_content = get_page_by_path($args['slug'],OBJECT,'patreon-content');
-
-			if($patreon_content == false) {
-				return 'Patreon content not found.';
-			}
-
-			if(current_user_can('manage_options')) {
-				return Patreon_Frontend::returnPatreonEmbeddedContent($patreon_content->post_content);
-			}
-
-			$patreon_level = get_post_meta( $patreon_content->ID, 'patreon-level', true );
-
-			if($patreon_level == 0) {
-				return Patreon_Frontend::returnPatreonEmbeddedContent($patreon_content->post_content);
-			}
-
-			$user_patronage = Patreon_Wordpress::getUserPatronage();
-
-			if($user_patronage != false) {
-
-				if(is_numeric($patreon_level) && $user_patronage >= ($patreon_level*100) ) {
-
-					return Patreon_Frontend::returnPatreonEmbeddedContent($patreon_content->post_content);
-
-				}
-
-			}
-
-			$embed_post = false;
-			if(isset($args['paywall_embed_post_slug']) && isset($args['paywall_embed_post_type'])) {
-
-				$post_slug = $args['paywall_embed_post_slug'];
-				$post_type = $args['paywall_embed_post_type'];
-				$post = get_page_by_path($post_slug, OBJECT, $post_type);
-
-				if(isset($post->ID)) {
-					$embed_post = Patreon_Frontend::returnPatreonEmbeddedContent($post->post_content);
-				} else {
-					$embed_post = false;
-				}
-
-			}
-
-			$embed_code = false;
-			if(isset($args['paywall_embed_url'])) {
-				$embed_code = wp_oembed_get( $args['paywall_embed_url']);
-			}
-
-			$image_url = false;
-			if(isset($args['paywall_image_url'])) {
-				$image_url = '<img src="'.$args['paywall_image_url'].'"/>';
-			}
-
-			if($embed_post) {
-				return $embed_post . '<br/>' . Patreon_Frontend::displayPatreonCampaignBanner($patreon_level);
-			} if($embed_code) {
-				return $embed_code . '<br/>' . Patreon_Frontend::displayPatreonCampaignBanner($patreon_level);
-			} else if ($image_url) {
-				return $image_url . '<br/>' . Patreon_Frontend::displayPatreonCampaignBanner($patreon_level);
-			} else {
-				return Patreon_Frontend::displayPatreonCampaignBanner($patreon_level);
-			}
-
-		}
-
-	}
 
 }
 
