d5e77ecea233d8923b564e2a9e418c044b5d5825
Ozgur Zeren <unity100@gmail.com>
2017-11-08 01:15:32 +0100

    Deprecated patrons_only shortcode and its function removed
    Deprecated patrons_only shortcode and its function removed

--- classes/patreon_shortcodes.php
+++ classes/patreon_shortcodes.php
@@ -13,42 +13,9 @@ class Patreon_Shortcodes {
 		add_shortcode( 'patreon_register_button', array($this, 'renderPatreonButton') );
 		add_shortcode( 'patrons_name', array($this, 'renderPatronsName') );
 		add_shortcode( 'personal_message', array($this, 'renderPersonalMessage') );
-		add_shortcode( 'patrons_only', array($this, 'patronsOnlyContent') );
 
 	}
 
-	public function patronsOnlyContent($args, $content = '') {
-
-		$user_patronage = Patreon_Wordpress::getUserPatronage();
-
-		$min_level = 0;
-		if(isset($args['min_level']) && is_numeric($args['min_level'])) {
-			$min_level = $args['min_level'];
-		}
-	
-		if($user_patronage == false || $user_patronage < ($min_level*100)) {
-
-			/* check if shortcode has slug parameter */
-			if(isset($args['slug'])) {
-
-				/* get patreon-content post with matching url slug */
-				$patreon_content = get_page_by_path($args['slug'],OBJECT,'patreon-content');
-
-				if($patreon_content == false) {
-					$content = 'Patreon content not found.';
-				} else {
-					$content = Patreon_Frontend::returnPatreonEmbeddedContent($patreon_content->post_content);
-				}
-
-			} else {
-				$content = Patreon_Frontend::displayPatreonCampaignBanner($min_level);
-			}
-
-		}
-
-		return $content;
-
-	}
 
 	public function renderPersonalMessage($args) {
 
