63bef950e7e6b1052ac06ae214ea73749ca85382
Ozgur Zeren <unity100@gmail.com>
2017-11-18 01:22:53 +0100

    fetch_rewards function in Patreon_API class to fetch only rewards
    Some less load on API this way, and also easier to process the return

--- classes/patreon_api.php
+++ classes/patreon_api.php
@@ -10,14 +10,18 @@ class Patreon_API {
 	public function __construct($access_token) {
 		$this->access_token = $access_token;
 	}
-
+	
 	public function fetch_user() {
 		return $this->__get_json("current_user");
 	}
-
 	public function fetch_campaign_and_patrons() {
 		return $this->__get_json("current_user/campaigns?include=rewards,creator,goals,pledges");
 	}
+	
+	public function fetch_rewards() {
+		return $this->__get_json("current_user/campaigns?include=rewards");
+	}
+	
 	public function fetch_creator_info() {
 		return $this->__get_json("current_user/campaigns?include=creator");
 	}
