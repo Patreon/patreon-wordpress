e7b1ec465175f1d4bad039a117bfc6432996e0ea
Ozgur Zeren <unity100@gmail.com>
2017-11-23 03:04:20 +0100

    Added text define to use in banner under post when Patron and patronage are valid
    Added text define to use in banner under post when Patron and patronage are valid

--- patreon.php
+++ patreon.php
@@ -31,6 +31,7 @@ define("PATREON_TEXT_LOCKED_POST", 'Sorry! This content is for our Patrons who p
 define("PATREON_TEXT_OVER_BUTTON_1", 'This content is available to Patrons pledging $%%pledgelevel%% or more on Patreon' );
 define("PATREON_TEXT_OVER_BUTTON_2", 'Edit your pledge to %%creator%% to $%%pledgelevel%% or more to access this content. You\'re currently pledging $%%currentpledgelevel%%.' );
 define("PATREON_TEXT_OVER_BUTTON_3", 'Your Patreon payment method appears to be declined. Update your pledge to access this post.' );
+define("PATREON_VALID_PATRON_POST_FOOTER_TEXT", 'This content is for Patrons pledging $%%pledgelevel%% or more on %%creatorprofileurl%%' );
 define("PATREON_TEXT_UNDER_BUTTON_1", '' );
 define("PATREON_TEXT_UNDER_BUTTON_2", 'Already pledging $%%pledgelevel%% or more? %%flowlink%% to access this post.' );
 define("PATREON_TEXT_UNDER_BUTTON_3", 'Already updated? %%flowlink%% to access this post.' );
