062de53696c23522f8542c0b2ea7d5f676a12c69
Ozgur Zeren <unity100@gmail.com>
2017-11-26 22:58:47 +0100

    patreonMakeLoginButton updated to use patreonMakeLoginLink
    patreonMakeLoginButton updated to use patreonMakeLoginLink

--- classes/patreon_frontend.php
+++ classes/patreon_frontend.php
@@ -518,13 +518,11 @@ class Patreon_Frontend {
 			
 		}
 		
-		$redirect_uri = site_url().'/patreon-authorization/';
+		$href = self::patreonMakeLoginLink($client_id);
 
-		$href = 'https://www.patreon.com/oauth2/authorize?response_type=code&client_id='.$client_id.'&redirect_uri='.$redirect_uri;
-	
 		return apply_filters('ptrn/login_button', '<a href="'.$href.'" class="ptrn-login" data-ptrn_nonce="' . wp_create_nonce( 'patreon-nonce' ).'"><div class="patreon-responsive-button-wrapper"><div class="patreon-responsive-button"><img class="patreon_logo" src="'.PATREON_PLUGIN_ASSETS.'/img/patreon-logomark-on-coral.svg" alt=""> '.$login_label.'</div></div></a>', $href);
 
-	}	
+	}
 	function protectContentFromUsers($content) {
 
 		global $post;
