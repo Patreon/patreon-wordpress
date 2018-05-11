fa716643ca5fe0f54b0d0388d6d99469bd3a0ca5
Ozgur Zeren <contact@codebard.com>
2018-05-11 23:58:46 +0200

    fetch_user function v2 compatibility
    - Function now uses v2 api if aailable
    - Function translates v2 return to v1 return to ensure compatibility
    - Function takes into account new last_charge_status to construct declined status
    - Line corrections

--- classes/patreon_api.php
+++ classes/patreon_api.php
@@ -11,9 +11,36 @@ class Patreon_API {
 		$this->access_token = $access_token;
 	}
 	
-	public function fetch_user() {
+	public function fetch_user($v2=false) {
+
+		// Below conditional and different endpoint can be deprecated to only use v2 api after transition period
+		if($v2 OR get_option('patreon-can-use-api-v2',false)=='yes') {
+		
+			// We construct the old return from the new returns by combining /me and pledge details
+
+			$api_return = $this->__get_json("me?include=memberships&fields[member]=currently_entitled_amount_cents,lifetime_support_cents,last_charge_status,patron_status,last_charge_date,pledge_relationship_start");
+			
+			$creator_id = get_option('patreon-creator-id', false);
+			
+			if(isset($api_return['included'][0]) AND is_array($api_return['included'][0])) {
+				
+				$api_return['included'][0]["relationships"]["creator"]["data"]["id"]=$creator_id;
+				$api_return['included'][0]['type']='pledge';
+				$api_return['included'][0]['attributes']['amount_cents']=$api_return['included'][0]['attributes']['currently_entitled_amount_cents'];
+				$api_return['included'][0]['attributes']['created_at']=$api_return['included'][0]['attributes']['pledge_relationship_start'];
+				
+			}
+			if($api_return['included'][0]['attributes']['last_charge_status']!='Paid') {
+				
+				$api_return['included'][0]['attributes']['declined_since']=$api_return['included'][0]['attributes']['last_charge_date'];
+			}
+
+			return $api_return;
+		}		
+
 		return $this->__get_json("current_user");
 	}
+	
 	public function fetch_campaign_and_patrons() {
 		return $this->__get_json("current_user/campaigns?include=rewards,creator,goals,pledges");
 	}
