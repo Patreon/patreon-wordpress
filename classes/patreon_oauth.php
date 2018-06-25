b7cd06ec9947937562089ff78667a26c200a2def
Ozgur Zeren <contact@codebard.com>
2018-06-25 19:18:51 +0200

    patreon_oauth.php wp standards reformat initial commit

--- classes/patreon_oauth.php
+++ classes/patreon_oauth.php
@@ -9,43 +9,41 @@ class Patreon_OAuth {
 	private $client_secret;
 
 	public function __construct() {
-		$this->client_id = get_option('patreon-client-id', false);
-		$this->client_secret = get_option('patreon-client-secret', false);
+		$this->client_id 	 = get_option( 'patreon-client-id', false );
+		$this->client_secret = get_option( 'patreon-client-secret', false );
 	}
 
-	public function get_tokens($code, $redirect_uri) {
-		return $this->__update_token(array(
-			"grant_type" => "authorization_code",
-			"code" => $code,
-			"client_id" => $this->client_id,
+	public function get_tokens( $code, $redirect_uri ) {
+		return $this->__update_token( array(
+			"grant_type" 	=> "authorization_code",
+			"code" 	     	=> $code,
+			"client_id"  	=> $this->client_id,
 			"client_secret" => $this->client_secret,
-			"redirect_uri" => $redirect_uri
-		));
+			"redirect_uri"  => $redirect_uri
+		) );
 	}
 
-	public function refresh_token($refresh_token, $redirect_uri) {
-		return $this->__update_token(array(
-			"grant_type" => "refresh_token",
+	public function refresh_token( $refresh_token, $redirect_uri ) {
+		return $this->__update_token( array(
+			"grant_type" 	=> "refresh_token",
 			"refresh_token" => $refresh_token,
-			"client_id" => $this->client_id,
+			"client_id" 	=> $this->client_id,
 			"client_secret" => $this->client_secret
-		));
+		) );
 	}
 
-	private function __update_token($params) {
+	private function __update_token( $params ) {
 		
 		$api_endpoint = "https://api.patreon.com/oauth2/token";
 	
 		$api_request = array(
 			'method' => 'POST',
-			'body' => $params,
+			'body'   => $params,
 		);
 
 		$response = wp_remote_post( $api_endpoint, $api_request );
 
-		return json_decode($response['body'], true);
+		return json_decode( $response['body'], true );
 
 	}
 }
-
-?>
