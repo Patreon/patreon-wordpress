b1f12c32b88687cb8a64d9291274346926d04384
codebard <contact@codebard.com>
2018-08-24 17:27:20 +0200

    Added version checker that hooks into WP update check routine. Added an admin side update available notice. Hook for version checker added

--- classes/patreon_wordpress.php
+++ classes/patreon_wordpress.php
@@ -54,6 +54,7 @@ class Patreon_Wordpress {
 		add_action( 'admin_notices', array( $this, 'AdminMessages' ) );
 		add_action( 'init', array( $this, 'transitionalImageOptionCheck' ) );
 		add_action( 'admin_init', array( $this, 'add_privacy_policy_section' ), 20 ) ;
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'check_for_update' ) );
 
 	}
 	public static function getPatreonUser( $user ) {
@@ -206,6 +207,7 @@ class Patreon_Wordpress {
 	public static function checkPatreonCreatorURL() {
 		
 		// Check if creator url doesnt exist. 
+		$creator_url = self::getPatreonCreatorURL();
 		
 		if ( !get_option( 'patreon-creator-url', false ) OR get_option( 'patreon-creator-url', false ) == '' ) {
 			
@@ -687,7 +689,7 @@ class Patreon_Wordpress {
 			update_option( 'patreon-file-locking-feature-notice-shown', 1 );
 			
 		}
-			
+		
 		if( !get_option( 'patreon-gdpr-notice-shown', false ) ) {
 			
 			?>
@@ -701,6 +703,33 @@ class Patreon_Wordpress {
 			
 		}
 		
+		if( get_option( 'patreon-wordpress-update-available', false ) ) {
+			
+			?>
+				 <div class="notice notice-info is-dismissible">
+				 <h3>New version of Patreon WordPress is available</h3>
+					<p>To be able to receive the latest features, security and bug fixes, please update your plugin by <a href="<?php echo wp_nonce_url( get_admin_url() . 'update.php?action=upgrade-plugin&plugin=' . PATREON_WORDPRESS_PLUGIN_SLUG,'upgrade-plugin_' . PATREON_WORDPRESS_PLUGIN_SLUG ); ?>">clicking here</a>.</p>
+				</div>
+			<?php
+			
+		}
+		
 	}
+	public function check_for_update($plugin_check_data) 
+	{
+		global $wp_version, $plugin_version, $plugin_base;
+
+		if ( empty( $plugin_check_data->checked ) ) {
+			return $plugin_check_data;
+		}
+
+		if ( isset( $plugin_check_data->response[PATREON_WORDPRESS_PLUGIN_SLUG] ) AND version_compare( PATREON_WORDPRESS_VERSION, $plugin_check_data->response[PATREON_WORDPRESS_PLUGIN_SLUG]->new_version, '<' ) ) {
+
+			update_option( 'patreon-wordpress-update-available', 1 );
+		}
+
+		return $plugin_check_data;
+		
+	}	
 	
 }
