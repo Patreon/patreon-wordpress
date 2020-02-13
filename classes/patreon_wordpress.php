c499bfe6003b489ff0ff1c01770e5cdf30d214c7
Codebard <contact@codebard.com>
2020-02-12 19:13:54 -0500

    Removed auto updating of tiers when creating the tier dropdown.

--- classes/patreon_wordpress.php
+++ classes/patreon_wordpress.php
@@ -1647,12 +1647,7 @@ class Patreon_Wordpress {
 		}
 		
 		// This function makes a select box with rewards and reward ids from creator's campaign to be used in post locking and site locking
-		
-		// First force an update of creator tiers from the api in case they were changed.
-		if ( !isset( $args['skip_tier_update_from_api'] ) ) {
-			self::update_creator_tiers_from_api();
-		}
-		
+				
 		// Get updated tiers from db
 		$creator_tiers = get_option( 'patreon-creator-tiers', false );
 
